4497a2246515c1cfd46cd334da9ce8e2c0ef0d07
Ty Allembert <tyallembert@gmail.com>
2024-05-28 23:06:23 -0400

    debug: fixed URL for deployment

--- src/app/products/[slug]/page.js
+++ src/app/products/[slug]/page.js
@@ -2,49 +2,48 @@ import React from 'react';
 import ProductPage from './ProductPage';
 
 async function getData(slug) {
-  // const url = new URL(process.env.FETCH_URL);
-  // url.pathname = `/api/products/getDetails`;
-  // const data = await fetch(url, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({ slug: slug })
-  // });
-  // if(!data.ok) {
-  //   console.error("Error fetching data")
-  //   return;
-  // }
-  // const dataJSON = await data.json();
-  // const images = dataJSON.productByHandle.images.edges.map((edge) => {
-  //   return {
-  //     id: edge.node.id,
-  //     src: edge.node.originalSrc,
-  //     alt: edge.node.altText,
-  //     width: edge.node.width,
-  //     height: edge.node.height
-  //   }
-  // })
-  // const variants = dataJSON.productByHandle.variants.edges.map((variant) => {
-  //   return {
-  //     id: variant.node.id,
-  //     options: variant.node.selectedOptions
-  //   }
-  // })
-  // const product = {
-  //   id: dataJSON.productByHandle.id,
-  //   title: dataJSON.productByHandle.title,
-  //   description: dataJSON.productByHandle.description,
-  //   handle: dataJSON.productByHandle.handle,
-  //   variantId: dataJSON.productByHandle.variants.edges[0].node.id,
-  //   variants: variants,
-  //   price: dataJSON.productByHandle.variants.edges[0].node.price.amount,
-  //   currencyCode: dataJSON.productByHandle.variants.edges[0].node.price.currencyCode,
-  //   options: dataJSON.productByHandle.options,
-  //   images: images,
+  const url = new URL(process.env.FETCH_URL);
+  url.pathname = `/api/products/getDetails`;
+  const data = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ slug: slug })
+  });
+  if(!data.ok) {
+    console.error("Error fetching data")
+    return;
+  }
+  const dataJSON = await data.json();
+  const images = dataJSON.productByHandle.images.edges.map((edge) => {
+    return {
+      id: edge.node.id,
+      src: edge.node.originalSrc,
+      alt: edge.node.altText,
+      width: edge.node.width,
+      height: edge.node.height
+    }
+  })
+  const variants = dataJSON.productByHandle.variants.edges.map((variant) => {
+    return {
+      id: variant.node.id,
+      options: variant.node.selectedOptions
+    }
+  })
+  const product = {
+    id: dataJSON.productByHandle.id,
+    title: dataJSON.productByHandle.title,
+    description: dataJSON.productByHandle.description,
+    handle: dataJSON.productByHandle.handle,
+    variantId: dataJSON.productByHandle.variants.edges[0].node.id,
+    variants: variants,
+    price: dataJSON.productByHandle.variants.edges[0].node.price.amount,
+    currencyCode: dataJSON.productByHandle.variants.edges[0].node.price.currencyCode,
+    options: dataJSON.productByHandle.options,
+    images: images,
   
-  // };
-  const product = null;
+  };
   return product;
 }
 async function ProductDetails({ params }) {
